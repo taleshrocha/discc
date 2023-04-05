ca3de050bb86615bb304f19aeb8d5bf26b55cbe6
taleshrocha <taleshrochaz@gmail.com>
2023-04-05 15:08:42 -0300

    3 Semester

---
+++ src/components/CustomNode.jsx
@@ -0,0 +1,25 @@
+import React, { memo, useState } from 'react';
+import { Handle, Position } from 'reactflow';
+
+function CustomNode({ data }) {
+  const [isDone, setIsDone] = useState(false)
+  return (
+    <button 
+      className={`flex flex-col px-2 py-1 w-44 h-20
+      shadow-md rounded-md border-2 border-stone-400 
+      ${isDone? "bg-neutral-100" : "bg-zinc-300"}
+    `}
+      onClick={() => setIsDone(!isDone)}
+    >
+      <div>
+          <p className="text font-bold">{data.code} ({data.hours})</p>
+          <p className="text-sm text-gray-500">{data.name}</p>
+      </div>
+
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} />
+    </button>
+  );
+}
+
+export default memo(CustomNode);
---
+++ src/disciplines.js
@@ -0,0 +1,86 @@
+export const disciplines = [
+  {
+    period: 1,
+    code: "DIM0151",
+    name: "Introdução às Técnicas de Programação",
+    hours: "90",
+  },
+  {
+    period: 1,
+    code: "DIM0154",
+    name: "Métodos de Estudo no Ensino Superior",
+    hours: "30",
+  },
+  {
+    period: 1,
+    code: "MAT0345",
+    name: "Cálculo Diferencial e Integral I",
+    hours: "90",
+  },
+  {
+    period: 1,
+    code: "MAT1503",
+    name: "Geometria Analítica e Vetorial",
+    hours: "60",
+  },
+  {
+    period: 1,
+    code: "DIM0152",
+    name: "Matemática para Computação I",
+    hours: "60",
+  },
+
+  {
+    period: 2,
+    code: "DIM0127",
+    name: "Arquitetura de Computadores",
+    hours: "60",
+  },
+  {
+    period: 2,
+    code: "DIM0119",
+    name: "Estruturas de Dados Básicas I",
+    hours: "60h",
+  },
+  {
+    period: 2,
+    code: "DIM0156",
+    name: "Introdução à Análise de Algoritmos",
+    hours: "60",
+  },
+  { period: 2, code: "DIM0176", name: "Programação I", hours: "60" },
+  { period: 2, code: "MAT0421", name: "Álgebra Linear", hours: "60" },
+  {
+    period: 2,
+    code: "DIM0155",
+    name: "Matemática para Computação II",
+    hours: "60",
+  },
+
+  { period: 3, code: "DIM0128", name: "Circuitos Lógicos", hours: "60" },
+  {
+    period: 3,
+    code: "DIM0117",
+    name: "Estrutura de Dados Básicas II",
+    hours: "60",
+  },
+  {
+    period: 3,
+    code: "DIM0161",
+    name: "Seminários de Computação e Orientação Curricular",
+    hours: "30",
+  },
+  {
+    period: 3,
+    code: "MAT0031",
+    name: "Cálculo Diferencial e Integral II",
+    hours: "60",
+  },
+  {
+    period: 3,
+    code: "EST0165",
+    name: "Introdução à Probabilidade",
+    hours: "60",
+  },
+  { period: 3, code: "DIM0157", name: "Introdução à Lógica", hours: "60" },
+];
