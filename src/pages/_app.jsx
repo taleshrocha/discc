f015fe93f76ff05d4aa400fe721b0c5676f38ff8
taleshrocha <taleshrochaz@gmail.com>
2023-04-11 17:59:56 -0300

    Get rid of unecessary stuff

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,23 +1,15 @@
-import React, { useCallback } from "react";
-import ReactFlow, {
-  useNodesState,
-  useEdgesState,
-  addEdge,
-  MiniMap,
-  Controls,
-} from "reactflow";
+import ReactFlow, { MiniMap, Controls } from "reactflow";
 import Head from "next/head";
 
 import "reactflow/dist/base.css";
 import "@/styles/globals.css";
 
 import CustomNode from "@/components/CustomNode";
-import { disciplines } from "@/disciplines.js"
+import { disciplines } from "@/disciplines.js";
 
 const nodeTypes = {
   custom: CustomNode,
 };
-
 
 let initNodes = [];
 let x, y;
@@ -25,18 +17,18 @@ x = y = 0;
 
 for (let id = 0; id < disciplines.length; id++) {
   if (disciplines[id].code == "DIM0127") {
-    x = 0
-    y = 300
-  }
-  else if(disciplines[id].code == "DIM0128"){
-    x = 0
-    y = 600
+    x = 0;
+    y = 300;
+  } else if (disciplines[id].code == "DIM0128") {
+    x = 0;
+    y = 600;
   }
   initNodes.push({
     id: id.toString(),
     type: "custom",
     data: disciplines[id],
     position: { x: x, y: y },
+    selectable: false,
   });
   x += 200;
 }
@@ -46,37 +38,29 @@ const initEdges = [
     id: "e0-5",
     source: "0",
     target: "5",
-    type: "step"
+    type: "step",
   },
   {
     id: "e0-6",
     source: "0",
     target: "6",
-    type: "step"
+    type: "step",
   },
   {
     id: "e0-7",
     source: "0",
     target: "7",
-    type: "step"
+    type: "step",
   },
   {
     id: "e0-8",
     source: "0",
     target: "8",
-    type: "step"
+    type: "step",
   },
 ];
 
-const Flow = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initEdges);
-
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    []
-  );
-
+export default function App() {
   return (
     <>
       <Head>
@@ -88,11 +72,14 @@ const Flow = () => {
 
       <main style={{ width: "100vw", height: "100vh" }}>
         <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
+          nodes={initNodes}
+          edges={initEdges}
+          onNodeClick={(e, node) => {
+            console.log("Node id:", node.id);
+            edges.forEach((edge) => {
+              console.log(edge.target == node.id);
+            });
+          }}
           nodeTypes={nodeTypes}
           fitView
           className="bg-teal-50"
@@ -103,6 +90,4 @@ const Flow = () => {
       </main>
     </>
   );
-};
-
-export default Flow;
+}
